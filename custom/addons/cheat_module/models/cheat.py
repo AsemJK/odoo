4177dd1280c3fbabea2bb9cd794d89daa2f1d404
asemjk <asemalsaiyadi@gmail.com>
2025-08-26 05:17:48 +0300

    save

--- custom/addons/cheat_module/models/cheat.py
+++ custom/addons/cheat_module/models/cheat.py
@@ -1,6 +1,25 @@
-from odoo import models
+import random
+import string
+
+
+from odoo import _,models,fields,api
 class BasicCheat(models.Model):
     _name = 'basic.cheat'
+    char_field = fields.Char(string='Char field',required=True)
+    text_field = fields.Text(string='Text field')
+    boolean_field = fields.Boolean(string='Boolean field')
+    integer_field = fields.Integer(string='Integer field')
+    selection_field = fields.Selection(selection=[
+        ('1','item1'),
+        ('2','item2'),
+        ('3','item3'),
+    ],string='Selection field')
+    computed_field = fields.Boolean(string='Computed field',compute='_my_compute_field',store=True)
+    @api.depends('boolean_field')
+    def _my_compute_field(self):
+        for record in self:
+            record.computed_field = record.boolean_field
+
     def __init__(self):
         pass
     def cheat(self):
